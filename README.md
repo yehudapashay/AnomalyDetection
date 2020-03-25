cfdc393de69c6fbe0d8effd6ab068451e39fcf11
yehudapashay <yehudapashay1@gmail.com>
2020-03-25 11:34:32 +0200

    [UPDATE]

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# AnomalyDetection<br>
+# AnomalyDetection
 
-Anomaly detection of aircraft data from ADS-B data set using machine learning models such as: LSTM , SVM and Isolation Forest.
+A system which is able to create offline machine learning models for anomaly detection. The system allows creation and loading of machine learning models by using dynamic inputs. Moreover, the system displays different output plots and evaluation metrics which compare between different models and the diagnosis of anomalies which were found. Running the system with dynamic parameters will allow us to extract many different machine learning models. Comparing them based on different evaluation metrics will lead to obtaining the best machine learning models for anomaly detection. Those models will be used as a baseline for a real-time & light-weight anomaly detection algorithm based on streaming data from UAV sensors in to order to get the earliest possible detection of GPS spoofing attacks on UAV’s.  
+
+## Getting Started
+
+First, you should clone the project to your local computer.
+1. Run 'guiController.py' file in order to run the system.
+2. Currently the flow of creating new model is working(load existing model will be available later).
+3. Enter valid inputs for train,test and results directories.
+4. Choose LSTM and edit the parameters in order to continue to next step in the system (LSTM is working, more algorithms will be available later).
+5. Skip the next step (will be available later).
+6. Choose cosine similarity function (optional).
+7. Run new model.
+
+### Prerequisites
+
+You should run the command (before run the system) in the console
+
+```
+pip install -r requirements.txt
+```
+
+## Built With
+
+* [PyCharm](https://www.jetbrains.com/pycharm/) - The Python IDE for Professional Developers
+* [Flightradar24](https://www.flightradar24.com/) -ADS-B data sets
+
+## Authors
+
+* **Lior Pizman** - *Prototype initial work* - [Github](https://github.com/liorpizman/)
+* **Yehuda Pashay** - *Prototype initial work* - [Github](https://github.com/yehudapashay)
+
+See also the list of [contributors](https://github.com/liorpizman/AnomalyDetection/contributors) who participated in this project.
+
--- gui/gui_controller.py
+++ gui/gui_controller.py
@@ -1,13 +1,19 @@
-import tkinter as tk
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
 from tkinter import font as tkfont
 
-from gui.algorithms_window import AlgorithmsWindow
-from gui.feature_selection_window import FeatureSelectionWindow
-from gui.load_model_window import LoadModel
-from gui.main_window import MainWindow
-from gui.model_controller import model_controller
-from gui.new_model_window import NewModel
-from gui.similarity_functions_window import SimilarityFunctionsWindow
+from gui.windows.algorithms_window import AlgorithmsWindow
+from gui.windows.existing_algorithms_window import ExistingAlgorithmsWindow
+from gui.windows.load_model_window import LoadModel
+from gui.windows.loading_window import LoadingWindow
+from gui.windows.parameters_options_window import ParametersOptionsWindow
+from gui.windows.main_window import MainWindow
+from utils.model_controller import ModelController
+from gui.windows.new_model_window import NewModel
+from gui.windows.results_window import ResultsWindow
+from gui.windows.similarity_functions_window import SimilarityFunctionsWindow
 
 
 class AnomalyDetectionGUI(tk.Tk):
@@ -15,7 +21,7 @@ class AnomalyDetectionGUI(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        self.model_controller = model_controller(self)
+        self.model_controller = ModelController(self)
 
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
         self.geometry('800x600')
@@ -27,11 +33,19 @@ class AnomalyDetectionGUI(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-        # container.title('Anomaly Detection Classifier')
-        # container.geometry("650x400")
         # container.option_add('*tearOff', 'FALSE')  # Disables ability to tear menu bar into own window
+        self.current_algorithm_to_edit = "LSTM"
+
         self.frames = {}
-        for F in (MainWindow, NewModel, LoadModel, AlgorithmsWindow, FeatureSelectionWindow, SimilarityFunctionsWindow):
+        for F in (MainWindow,
+                  NewModel,
+                  LoadModel,
+                  AlgorithmsWindow,
+                  SimilarityFunctionsWindow,
+                  ExistingAlgorithmsWindow,
+                  LoadingWindow,
+                  ResultsWindow,
+                  ParametersOptionsWindow):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -48,13 +62,19 @@ class AnomalyDetectionGUI(tk.Tk):
         frame = self.frames[page_name]
         frame.tkraise()
 
+    def reinitialize_frame(self, page_name):
+        '''Reinitialize_frame a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.reinitialize()
+        frame.tkraise()
+
     def set_new_model_training_input_path(self, input):
         self.model_controller.set_training_data_path(input)
 
     def set_new_model_test_input_path(self, input):
         self.model_controller.set_test_data_path(input)
 
-    def set_new_model_results_input_path(self,input):
+    def set_new_model_results_input_path(self, input):
         self.model_controller.set_results_path(input)
 
     def set_algorithm_parameters(self, algorithm_name, algorithm_parameters):
@@ -71,6 +91,39 @@ class AnomalyDetectionGUI(tk.Tk):
 
     def run_models(self):
         self.model_controller.run_models()
+
+    def set_new_model_running(self, new_model_running):
+        self.model_controller.set_new_model_running(new_model_running)
+
+    def set_existing_algorithms(self, algorithms_dict):
+        self.model_controller.set_existing_algorithms(algorithms_dict)
+
+    def set_existing_algorithms_threshold(self, threshold):
+        self.model_controller.set_existing_algorithms_threshold(threshold)
+
+    def set_features_columns_options(self):
+        self.model_controller.set_features_columns_options()
+
+    def get_features_columns_options(self):
+        return self.model_controller.get_features_columns_options()
+
+    def set_users_selected_features(self, features_list):
+        self.model_controller.set_users_selected_features(features_list)
+
+    def add_new_thread(self, new_thread):
+        self.model_controller.add_new_thread(new_thread)
+
+    def get_existing_thread(self):
+        return self.model_controller.get_existing_thread()
+
+    def get_new_model_running(self):
+        return self.model_controller.get_new_model_running()
+
+    def set_current_algorithm_to_edit(self, algorithm_name):
+        self.current_algorithm_to_edit = algorithm_name
+
+    def get_current_algorithm_to_edit(self):
+        return self.current_algorithm_to_edit
 
 
 if __name__ == "__main__":
